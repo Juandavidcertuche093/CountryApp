d1633e040f3de2281e1176b29fc481dac3e5c9eb
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-12-13 19:11:41 -0500

    Update app module; add new country interface and countries service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
+
 
 
 @NgModule({
@@ -14,7 +17,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  //PROPIEDADES
+  private apiUrl: string = 'https://restcountries.com/v3.1'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  //METODO
+  searchCapital( term: string):Observable<Country[]>{
+    return this.http.get<Country[]>(`${this.http}/capital/${ term }`);
+  }
+}
