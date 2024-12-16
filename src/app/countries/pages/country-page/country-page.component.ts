02d33d95f36d5cd4d88009f0f9766e7153cc927b
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-12-16 10:51:52 -0500

    Update by-country, by-region, and country pages; modify countries service

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
 
+  country?: Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,//proporciona acceso a información sobre la ruta activa, como parámetros, fragmentos, y datos asociados.
+    private router: Router,
+    private countriesService: CountriesService,
+
+  ){}
+
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode(id)),
+    )
+    .subscribe( country => {
+
+      if ( !country ) {
+        return this.router.navigateByUrl('');
+      }
+
+      console.log('TENEMOS UN PAIS')
+
+      return
+    })
+
+  }
 }
